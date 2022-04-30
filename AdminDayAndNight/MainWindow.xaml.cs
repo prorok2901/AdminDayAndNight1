41d68b0990d5d4c2cd8967778e5c9f19f3d474b0
nekto <nigma-ruslan2002@mail.ru>
2022-04-30 11:01:42 +0500

    я почти не ленился

--- AdminDayAndNight/MainWindow.xaml.cs
+++ AdminDayAndNight/MainWindow.xaml.cs
@@ -15,14 +15,30 @@ using System.Windows.Shapes;
 
 namespace AdminDayAndNight
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        BD.group363_06Entities appapa;
+        Registration_S_User user;
+        Authorization authorization;
         public MainWindow()
         {
             InitializeComponent();
         }
+        private void chekSuperUser()
+        {
+            BD.user super_admin =appapa.user.FirstOrDefault(a => a.role != null && a.role == "Super_User");
+            if(super_admin == null)
+            {
+                user = new Registration_S_User();
+                Opupet.Navigate(user);
+            }
+            else
+            {
+                authorization = new Authorization();
+                Opupet.Navigate(authorization);
+            }
+        }
     }
 }
+
