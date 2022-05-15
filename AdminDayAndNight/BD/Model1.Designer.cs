a9b843c392ad010a4a3dfa6d6d7599243650ceb0
prorok2901@gmail.com <prorok2901@gmail.com>
2022-05-15 20:58:39 +0500

    переделка дизайна

--- AdminDayAndNight/BD/Model1.Designer.cs
+++ AdminDayAndNight/BD/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\proro\source\repos\prorok2901\AdminDayAndNight1\AdminDayAndNight\BD\Model1.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\proro\Source\Repos\prorok2901\AdminDayAndNight1\AdminDayAndNight\BD\Model1.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
